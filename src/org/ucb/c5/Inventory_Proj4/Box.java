327b8bc58a4154195c24f0542cfc20a8315637d3
arjunchandran1999 <36093682+arjunchandran1999@users.noreply.github.com>
2019-11-17 14:17:57 -0800

    Add files via upload

---
+++ src/org/ucb/c5/Inventory_Proj4/Box.java
@@ -0,0 +1,25 @@
+public class Box{
+	private Hashmap<Composition, Hashmap<Concentration, location>> everythings_loc = new Hashmap<Conposition, Hashmap<Concentration, location>>;
+	private Location box_loc;
+	private List<Location> emptyspots = new Arraylist<>();
+	private Name box_name;
+	private Experimentalthrd currthrd;
+
+	public void Box(Hashmap<Concentration, Hashmap<Concentration, Location>> data, Location boxs_loc, )
+	public Location get(Location loc){
+
+	}
+	public void put(){
+
+	}
+	public getlab_loc(){
+
+	}
+	public containsName(){
+
+	}
+	public getbox_name(){
+
+	}
+
+}
