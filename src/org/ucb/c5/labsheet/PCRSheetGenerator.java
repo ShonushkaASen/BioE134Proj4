45c91ac1d4d9cf4379925e89158886745a11b145
sghandian <sghandian@berkeley.edu>
2019-11-17 14:46:24 -0800

    PCRSheetGenerator v1.2

--- src/org/ucb/c5/labsheet/PCRSheetGenerator.java
+++ src/org/ucb/c5/labsheet/PCRSheetGenerator.java
@@ -7,17 +7,23 @@ import java.util.List;
 
 public class PCRSheetGenerator { //, Inventory
     public static String run(List<Step> steps) {
-        String samples = null;
-        String source = null;
-        String destination = null;
-        String notes1 = null;
-        String dilutionDests = null;
-        String notes2 = null;
+        StringBuilder samples = new StringBuilder();
+        samples.append("samples: \n label\tprimer1\tprimer2\ttemplate\tproduct\n");
+        StringBuilder source = new StringBuilder();
+        source.append("source: \n label\tlocation\tnote\n");
+        StringBuilder destination = new StringBuilder();
+        destination.append("destination:");
+        
+        StringBuilder notes1 = new StringBuilder();
+        StringBuilder dilutionDests = new StringBuilder();
+        StringBuilder notes2 = new StringBuilder();
 
         for (Step step: steps) {
             PCR pcrstep = (PCR) step;
             //creating separate strings for each heading within the PCRSheet
-            //samples.append(step.get(samples))
+            samples.append((pcrstep.getOligo1()));
+            samples.append((pcrstep.getOligo2()));
+
             //source.add(step.get(source))
             //destination.add(step.get(destination))
             //notes1 = ...
@@ -25,7 +31,9 @@ public class PCRSheetGenerator { //, Inventory
 
             //
         }
-        return null;
+        String PCRSheet = samples.toString() + source.toString() +
+                destination.toString() + notes1.toString() + dilutionDests.toString() + notes2.toString();
+        return PCRSheet;
     }
 
 
