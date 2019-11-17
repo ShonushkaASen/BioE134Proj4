7ec568f9073780d39f5025967326b6ff142aa95f
sghandian <sghandian@berkeley.edu>
2019-11-17 14:04:54 -0800

    PCRSheetGenerator 1.1

--- src/org/ucb/c5/labsheet/PCRSheetGenerator.java
+++ src/org/ucb/c5/labsheet/PCRSheetGenerator.java
@@ -5,10 +5,25 @@ import org.ucb.c5.constructionfile.model.Step;
 
 import java.util.List;
 
-public class PCRSheetGenerator {
+public class PCRSheetGenerator { //, Inventory
     public static String run(List<Step> steps) {
+        String samples = null;
+        String source = null;
+        String destination = null;
+        String notes1 = null;
+        String dilutionDests = null;
+        String notes2 = null;
+
         for (Step step: steps) {
             PCR pcrstep = (PCR) step;
+            //creating separate strings for each heading within the PCRSheet
+            //samples.append(step.get(samples))
+            //source.add(step.get(source))
+            //destination.add(step.get(destination))
+            //notes1 = ...
+            //dilutionDest
+
+            //
         }
         return null;
     }
