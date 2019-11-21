68182c5cb9836d1133f973f9b18b8bc8e61a0154
Arjun Chandran <arjun_chandran@berkeley.edu>
2019-11-21 15:02:52 -0800

    0a

---
+++ src/org/ucb/c5/Inventory/Experimentalthrd.java
@@ -0,0 +1,11 @@
+package org.ucb.c5.Inventory;
+
+public class Experimentalthrd {
+    private String thrd;
+    public Experimentalthrd(String Thrd){
+        this.thrd = Thrd;
+    }
+    public String get(){
+        return this.thrd;
+    }
+}
