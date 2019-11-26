6a125546eb8cb83e09ac86b74d2ae422c365d71d
ShonushkaASen <shonushka.sen@berkeley.edu>
2019-11-25 20:42:28 -0800

    fixed assemble

--- src/org/ucb/c5/labsheet/AssembleSheetGenerator.java
+++ src/org/ucb/c5/labsheet/AssembleSheetGenerator.java
@@ -2,8 +2,6 @@ package org.ucb.c5.labsheet;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-
-import org.ucb.c5.constructionfile.model.Assembly;
 import org.ucb.c5.constructionfile.model.Digestion;
 import org.ucb.c5.constructionfile.model.Step;
 
@@ -28,10 +26,9 @@ public class AssembleSheetGenerator {
             source.append("source \n dna\tlocation\n");
             destination = new StringBuilder();
             destination.append("destination:");
-            samples  = new StringBuilder();
+            samples = new StringBuilder();
             samples.append("samples \n label\tfragments\tproduct\n");
             fragments = new StringBuilder();
-         //   finalAssemble = new StringBuilder();
           
         } else {
             source = new StringBuilder(currStrings.get(0));
@@ -43,24 +40,21 @@ public class AssembleSheetGenerator {
         String dna = thread + current_sample_num + "p";
         String label = thread;
         fragments.append(dna).append(",");
-        String fragString = fragments.toString();
+        String fragString = fragments.toString()
         int fragStringlength = fragString.length();
         
         Assembly assembly = (Assembly) step;
-        //String product = String.format("oligo2-oligo1/%s", digestion.getProduct());
         String product = "";
-        //String source = "box" + thread + "/" + thread + Integer.toString(current_sample_num);
         String location = inventory.get(product, -1.0).getKey();
         source.append(dna + "\t").append(location).append("\n");
-        //samples.append(dna + "\t").append(label + "p\t").append(source + "\t").append(product + "\n");
-        samples.append(label + "\t").append(fragString.substring(0, fragStringlength - 1) + "\t")
+        samples.append.(label + "\t").append(fragString.substring(0, fragStringlength - 1) + "\t")
             .append(assembly.getProduct() + "\n");
             
                 
         
         System.out.println("Assembly:");
         System.out.println(samples.toString());
-        List<String> newCurrStrings = new ArrayList<String>(Arrays.asList(source.toString(), samples.toString(), destination.toString()));
+        List<String> newCurrStrings = new ArrayList<String>(Arrays.asList(source.toString(), samples.toString(); destination.toString()));
         return newCurrStrings;
     }
 }
